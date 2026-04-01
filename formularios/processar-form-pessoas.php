0337c5b31f62bf96fb878cf5eefdebfb594d2028
Pablo Theves <pablotheves20@gmail.com>
2026-04-01 20:25:19 -0300

    formularios

---
+++ formularios/processar-form-pessoas.php
@@ -0,0 +1,25 @@
+<?php
+
+    $nomeRecebido = $_POST['containerNome'];
+    $SobrenomeRecebido = $_POST['containerSobrenome'];
+    $IdadeRecebido = $_POST['containerIdade'];
+    $PesoRecebido = $_POST['containerPeso'];
+    $AlturaRecebido = $_POST['containerAltura'];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Dados da Pessoa</h3>
+    <p><strong>Nome:</strong> <?php echo $nomeRecebido; ?></p>
+    <p><strong>Sobrenome:</strong> <?php echo $SobrenomeRecebido; ?></p>
+    <p><strong>Idade:</strong> <?php echo $IdadeRecebido; ?></p>
+    <p><strong>Peso:</strong> <?php echo $PesoRecebido; ?></p>
+    <p><strong>Altura:</strong> <?php echo $AlturaRecebido; ?></p>
+</body>
+</html>
