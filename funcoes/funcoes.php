04561df922b3b49de775412eb52ccb79935f4d1a
Tiago <tiagol.r.goncalves@gmail.com>
2026-04-01 21:58:15 -0300

    Criando listar imcs

---
+++ funcoes/funcoes.php
@@ -0,0 +1,30 @@
+<?php
+
+
+
+//Dados IMC
+
+//Qual o IMC de cada participante?
+
+
+function listarImcs(mysqli $conexao): void
+{
+
+    $comandoSQL = "SELECT * from pessoas";
+    $retornoBanco = mysqli_query($conexao, $comandoSQL) or die(mysqli_error($conexao));
+
+    if (mysqli_num_rows($retornoBanco) > 0) {
+        echo "Nome - Sobrenome - IMC<br>";
+
+        while ($registro = mysqli_fetch_array($retornoBanco)) {
+            echo $registro['idpessoa'] .
+                " " . $registro['nome'] .
+                " " . $registro['sobrenome'] .
+                " " . $registro['peso/altura'] ."<br>";
+        }
+    } else {
+        echo "Nenhum resultado.";
+    }
+
+    return;
+}
