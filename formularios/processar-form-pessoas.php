a18f503b90ede913daec0f1ff20b93a650bd5e89
Pablo Theves <pablotheves20@gmail.com>
2026-04-01 21:08:17 -0300

    form inserir /processar pessoa

--- formularios/processar-form-pessoas.php
+++ formularios/processar-form-pessoas.php
@@ -1,25 +1,34 @@
 <?php
 
-    $nomeRecebido = $_POST['containerNome'];
-    $SobrenomeRecebido = $_POST['containerSobrenome'];
-    $IdadeRecebido = $_POST['containerIdade'];
-    $PesoRecebido = $_POST['containerPeso'];
-    $AlturaRecebido = $_POST['containerAltura'];
+$nomeRecebido = $_POST['containerNome'];
+$SobrenomeRecebido = $_POST['containerSobrenome'];
+$IdadeRecebido = $_POST['containerIdade'];
+$PesoRecebido = $_POST['containerPeso'];
+$AlturaRecebido = $_POST['containerAltura'];
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dados da Pessoa</title>
+    <link rel="stylesheet" href="../assets/estilo-forms.css">
 </head>
-<body>
-    <h3>Dados da Pessoa</h3>
-    <p><strong>Nome:</strong> <?php echo $nomeRecebido; ?></p>
-    <p><strong>Sobrenome:</strong> <?php echo $SobrenomeRecebido; ?></p>
-    <p><strong>Idade:</strong> <?php echo $IdadeRecebido; ?></p>
-    <p><strong>Peso:</strong> <?php echo $PesoRecebido; ?></p>
-    <p><strong>Altura:</strong> <?php echo $AlturaRecebido; ?></p>
-</body>
+<main class="container">
+
+    <body class="form-grid">
+        <h3>Dados da Pessoa</h3>
+        <p><strong>Nome:</strong> <?php echo $nomeRecebido; ?></p>
+        <p><strong>Sobrenome:</strong> <?php echo $SobrenomeRecebido; ?></p>
+        <p><strong>Idade:</strong> <?php echo $IdadeRecebido; ?></p>
+        <p><strong>Peso:</strong> <?php echo $PesoRecebido; ?></p>
+        <p><strong>Altura:</strong> <?php echo $AlturaRecebido; ?></p>
+    </body>
+
+</main>
+
+
 </html>
