3f504737dec89518e4aac667cd0b3384d8fb3157
Pablo Theves <pablotheves20@gmail.com>
2026-04-08 20:15:33 -0300

    testes

--- formularios/alterar-dados.php
+++ formularios/alterar-dados.php
@@ -1,11 +1,11 @@
 <?php
-
     include_once "../funcoes/funcoes.php";
-        
+
     $auxConectar = conectar();
 
     $sql = "SELECT * FROM pessoas";
     $resultado = mysqli_query($auxConectar, $sql);
+
 ?>
 
 <!DOCTYPE html>
@@ -13,14 +13,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../assets/estilo-forms.css">
+    <title>Alterar dados</title>
 </head>
 <body>
-    <main class="container-painel"> 
-        <h2>Lista dos participantes</h2>
-            <?php listarPessoas($auxConectar); ?>
-        </table>
-    </main>
+    
 </body>
 </html>
